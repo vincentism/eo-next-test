c55d4816b21fee85b39838091ef1320deef8d5b5
vincentism <hkustliwenqian@hotmail.com>
2025-10-20 15:00:11 +0800

    feat: test

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  trailingSlash: false
+  trailingSlash: true
 };
 
 export default nextConfig;
